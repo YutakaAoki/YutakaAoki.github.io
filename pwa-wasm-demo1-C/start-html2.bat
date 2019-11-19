82ba5c816264623774baf2efc2d53bf10e0162ec
YutakaAoki <KHF12570@nifty.com>
2019-11-19 13:08:31 +0900

    pwa-wasm-demo1-B changed11

---
+++ pwa-wasm-demo1-C/start-html2.bat
@@ -0,0 +1,8 @@
+@rem start chrome.exe /high-dpi-support=1 /force-device-scale-factor=1 http://localhost:8080/Kidou.html
+
+@rem start chrome.exe http://localhost:8080/Haifu/demo1/index.html
+@rem start chrome.exe --user-data-dir --unsafety-treat-insecure-origin-as-secure ./index.html
+
+start chrome.exe --user-data-dir --unsafety-treat-insecure-origin-as-secure ^
+http://localhost:8080/Haifu/pwa-wasm-demo1-C/index.html
+
