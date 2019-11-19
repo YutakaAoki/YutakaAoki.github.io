e53b8b4dd229da49d6a06e5b6dd3517de661f50c
YutakaAoki <KHF12570@nifty.com>
2019-11-20 08:59:41 +0900

    pwa-wasm 24

--- pwa-wasm-demo1-C-2019-11-19 - 3/start-html.bat
+++
@@ -1,5 +0,0 @@
-@rem start chrome.exe /high-dpi-support=1 /force-device-scale-factor=1 http://localhost:8080/Kidou.html
-
-@rem start chrome.exe http://localhost:8080/Haifu/demo1/index.html
-start chrome.exe http://localhost:8080/Haifu/pwa-wasm-demo1-C/index.html
-
