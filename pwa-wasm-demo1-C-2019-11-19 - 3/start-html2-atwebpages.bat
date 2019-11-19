1d383342d8830c335542e964753dcfcbe7d97867
YutakaAoki <KHF12570@nifty.com>
2019-11-20 08:58:19 +0900

    pwa-wasm 23

---
+++ pwa-wasm-demo1-C-2019-11-19 - 3/start-html2-atwebpages.bat
@@ -0,0 +1,7 @@
+@rem start chrome.exe /high-dpi-support=1 /force-device-scale-factor=1 http://localhost:8080/Kidou.html
+
+@rem start chrome.exe http://localhost:8080/Haifu/demo1/index.html
+@rem start chrome.exe --user-data-dir --unsafety-treat-insecure-origin-as-secure ./index.html
+
+start chrome.exe --user-data-dir --unsafety-treat-insecure-origin-as-secure http://nowsmartsoft.atwebpages.com/pwa-wasm-demo1-B/
+
